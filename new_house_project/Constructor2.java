116f0c0625ccbc2b308d9e67113b33a729ee462e
crashdev226 <midashand44@gmail.com>
2023-05-29 23:53:10 +0530

    Add files via upload

---
+++ new_house_project/Constructor2.java
@@ -0,0 +1,10 @@
+public class Constructor2 {
+    public static void main(String[] args) {
+        /******* boolean b=false *******/
+        String b = "false";
+        switch (b) {// n2
+            case "false":
+                System.out.print("a");
+        }
+    }
+}
---
+++ new_house_project/public class Constructor2{.java
@@ -0,0 +1,10 @@
+public class Sample{
+	public static void main(String[] args) {
+		/***boolean b=false******/
+		String b="false";
+		switch(b) {//n2
+		case "false" :		
+		System.out.print("a");
+		}
+	}
+}
